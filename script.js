ec29b4b038be5cd94e614809528aae7810fb931e
hushnudabdulazizov44-lang <hushnudabdulazizov44@gmail.com>
2026-08-05 16:24:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,16 @@ function updateCountdown() {
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerHTML = "❤";
+
+    heart.style.left = Math.random() * window.innerWidth + "px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 6000);
+}
